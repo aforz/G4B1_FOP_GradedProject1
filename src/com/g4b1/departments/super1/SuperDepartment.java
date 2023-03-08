e3f4235112b160b91277d26e4c82011e19108a01
Afroz Alam <afroz3002@gmail.com>
2023-03-08 05:39:00 +0530

    initial commit for assignment 1

---
+++ src/com/g4b1/departments/super1/SuperDepartment.java
@@ -0,0 +1,20 @@
+package com.g4b1.departments.super1;
+
+public class SuperDepartment {
+
+    public String departmentName() {
+        return "Super Department";
+    }
+
+    public String getTodaysWork() {
+        return "No Work as of now";
+    }
+
+    public String getWorkDeadline() {
+        return "Nil";
+    }
+
+    public String isTodayAHoliday() {
+        return "Today is not a holiday";
+    }
+}
