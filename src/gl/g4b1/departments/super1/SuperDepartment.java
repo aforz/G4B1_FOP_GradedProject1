19eff316a1cac9da7b59bfb4570c5b5fcfe274a1
Afroz Alam <afroz3002@gmail.com>
2023-03-11 09:32:24 +0530

    source code updated

--- src/gl/g4b1/departments/super1/SuperDepartment.java
+++ src/gl/g4b1/departments/super1/SuperDepartment.java
@@ -1,5 +1,7 @@
 package gl.g4b1.departments.super1;
 
+//super is a keyword so that we can not use this name for naming convention for variable, method, package, etc names.
+// it has been kept super1.
 public class SuperDepartment {
 
     public String departmentName() {
